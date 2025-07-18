dededec84093341e7900532079a6beab19b1fba9
Matt Holloway <hollowmatt@gmail.com>
2025-07-18 09:01:37 +0100

    more file input fun

--- script5.py
+++ script5.py
@@ -1,1 +1,6 @@
 import fileinput
+for line in fileinput.input():
+  if fileinput.isfirstline():
+    print(f"Processing file: {fileinput.filename()}")
+  if not line.startswith("##"):
+    print(line, end="")
